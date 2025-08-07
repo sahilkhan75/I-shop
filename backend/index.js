0f3f09787b90dcf35895bab034aad5cb775a842c
sahilkhan75 <sahilkhangoryan@gmail.com>
2025-08-07 11:47:20 +0530

    fix: use dynamic PORT for Render

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const categoryRouter = require("./router/categoryRouter");
 const colorRouter = require("./router/colorRouter");
 const productRouter = require("./router/productRouter");
@@ -9,31 +10,28 @@ const adminRouter = require("./router/adminRouter");
 const UserRouter = require('./router/userRouter');
 const cartRouter = require('./router/cartRouter');
 const orderRouter = require('./router/orderRouter');
+
 const server = express();
-server.use(cors())
+server.use(cors());
 server.use(express.json());
+
 server.use("/category", categoryRouter);
 server.use("/color", colorRouter);
-server.use("/product", productRouter)
-server.use("/admin", adminRouter)
-server.use("/user", UserRouter)
-server.use("/cart", cartRouter)
-server.use("/order", orderRouter)
+server.use("/product", productRouter);
+server.use("/admin", adminRouter);
+server.use("/user", UserRouter);
+server.use("/cart", cartRouter);
+server.use("/order", orderRouter);
 server.use(express.static("./public"));
 
-mongoose.connect(process.env.MONGODB, { dbName: 'WSJP87' }).then(
-    (res) => {
-        server.listen(5000, () => {
-            console.log("Server in runng on port no. 5000");
-        }
-        )
-        console.log("Connected to Mongodb ");
 
-    }
-).catch(
-    (err) => {
-        console.log("Error connected to mongodb", err);
+const PORT = process.env.PORT || 5000;
 
-    }
-);
-
+mongoose.connect(process.env.MONGODB, { dbName: 'WSJP87' }).then(() => {
+    server.listen(PORT, () => {
+        console.log(` Server running on port ${PORT}`);
+    });
+    console.log(" Connected to MongoDB");
+}).catch((err) => {
+    console.log(" Error connecting to MongoDB:", err);
+});
